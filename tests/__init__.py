40c8ad55cdbcf4b8e43e3db1f1e46a4d19be0c68
Emily Neilan <emilyneilan@gmail.com>
2026-05-29 12:28:51 -0400

    added default files

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+def test_addition(a: float, b: float) -> float:
+    return a + b
+
+def test_subtraction(a: float, b: float) -> float:
+    return a - b
+
+def test_multiplication(a: float, b: float) -> float:
+    return a * b
